cddb42a72d8b1b77b1d9957426a3a05033a1bb03
fcabrera-c <75145601+fcabrera-c@users.noreply.github.com>
2021-02-11 16:56:30 -0600

    Update Search-steps.js

--- nextme/src/test/resources/steps/Search-steps.js
+++ nextme/src/test/resources/steps/Search-steps.js
@@ -1,11 +1,12 @@
 Given(/^we select option "([^"]*)" in the bar menu$/, function(arg1) {
-  $("#__next > div > header > div.ng-24.styles-module_ng__r3HCi > div.ngi--6--15-16--1- > div > nav > ul > li:nth-child(6) > div > div > div > div.styles-module_nav-item__container__SsGye.styles-module_nav-item__container--header__1c0hi > i").click();
-  $(":root").waitTime(1, timeUnits.SECONDS);
+  $("#__next > div > header > div.ng-24.styles-module_ng__r3HCi > div.styles_logo-container__12fJG.ngi--2-4--6-5--1- > a > div").click();
+  $(":root").waitTime(2, timeUnits.SECONDS);
+  $("li.undefined:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)").moveTo();
   
 });
 Given(/^we click the option "([^"]*)"$/, function(arg1) {
-  $("#__next > div > header > div.ng-24.styles-module_ng__r3HCi > div.ngi--6--15-16--1- > div > nav > ul > li:nth-child(6) > div > div > div > div.styles-module_dropdown__content__3IGDs > ul > li:nth-child(1)").click();
-  $(":root").waitTime(1, timeUnits.SECONDS);
+  $("li.undefined:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1)").moveTo();
+  $("li.undefined:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1)").click();
   
 });
 
@@ -15,17 +16,17 @@ Given(/^we select the search bar and type something$/, function() {
   $("#duvidas-content > div > div > div > div.styles_duvidas-content__search-area__1uE1D.ngi--3-12--22-22-- > div.styles_duvidas-content__search-wrapper__2Ra2e > div > div > input").sendKeys("g");
   $("#duvidas-content > div > div > div > div.styles_duvidas-content__search-area__1uE1D.ngi--3-12--22-22-- > div.styles_duvidas-content__search-wrapper__2Ra2e > div > div > input").sendKeys("o");
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo("C:/Users/Anzen/Documents/Minium/Evidencias Web/type.png");
+ 
   $(":root").waitTime(2, timeUnits.SECONDS);
 });
 
 Given(/^we confirm the searching$/, function() {
   $("#duvidas-content > div > div > div > div.styles_duvidas-content__search-area__1uE1D.ngi--3-12--22-22-- > div.styles_duvidas-content__search-wrapper__2Ra2e > div").scrollIntoView();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo("C:/Users/Anzen/Documents/Minium/Evidencias Web/search1.png");
+  
   $(":root").waitTime(1, timeUnits.SECONDS);
   $("#duvidas-content > div > div > div > div.styles_duvidas-content__search-area__1uE1D.ngi--3-12--22-22-- > div.styles_duvidas-content__questions-accordion__1DV2G > div:nth-child(3)").scrollIntoView();
   $(":root").waitTime(1, timeUnits.SECONDS);
-  browser.screenshot().saveTo("C:/Users/Anzen/Documents/Minium/Evidencias Web/search2.png");
+  
   $(":root").waitTime(1, timeUnits.SECONDS);
 });
